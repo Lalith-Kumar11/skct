798cc0c5d828e4e1725346e21ac6de4461e8edfd
Lalith-Kumar11 <officiallalithkumar@gmail.com>
2024-12-15 17:49:56 +0530

    project updation

---
+++ files/alertbox.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>JavaScript Alert</h2>
+
+<button onclick="myFunction()">Try it</button>
+
+<script>
+function myFunction() {
+  alert("Move to Main Webpage");
+}
+</script>
+
+</body>
+</html>
